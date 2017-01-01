a3ed3e519d2cc98cc7f44f0ad73c745f76995480
Abbasha <m.bombus@yahoo.com>
2017-01-01 14:59:01 +0330

    Update setrank.lua

--- plugins/setrank.lua
+++ plugins/setrank.lua
@@ -1,5 +1,5 @@
 do
-local Dev = 122774063 --put your id here(BOT OWNER ID)
+local Dev = 92247090 --put your id here(BOT OWNER ID)
 
 local function setrank(msg, name, value) -- setrank function
   local hash = nil
@@ -43,7 +43,7 @@ local function res_user_callback(extra, success, result) -- /info <username> fun
   local um_hash = 'msgs:'..result.id..':'..extra.chat2
   user_info_msgs = tonumber(redis:get(um_hash) or 0)
   text = text..'تعداد پیام های فرستاده شده : '..user_info_msgs..'\n\n'
-  text = text..'SBSS Team'
+  text = text..'Bax Offline Bot'
   send_msg(extra.receiver, text, ok_cb,  true)
   else
 	send_msg(extra.receiver, extra.user..' نام کاربری مورد نظر یافت نشد.', ok_cb, false)
@@ -121,7 +121,7 @@ local function action_by_reply(extra, success, result)-- (reply) /info  function
   local um_hash = 'msgs:'..result.from.id..':'..result.to.id
   user_info_msgs = tonumber(redis:get(um_hash) or 0)
   text = text..'تعداد پیام های فرستاده شده : '..user_info_msgs..'\n\n'
-  text = text..'SBSS Team'
+  text = text..'Bax Offline Bot'
   send_msg(extra.receiver, text, ok_cb, true)
 end
 
@@ -194,7 +194,7 @@ local function run(msg, matches)
 	 text = text..'نام گروه : '..msg.to.title..'\n'
      text = text..'ایدی گروه : '..msg.to.id
     end
-	text = text..'\n\nSBSS Team'
+	text = text..'\n\nBax Offline Bot'
     return send_msg(receiver, text, ok_cb, true)
     end
   end
